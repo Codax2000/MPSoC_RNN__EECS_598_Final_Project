8c7b8095b9a8098986afbd622fcba683db42f58a
Codax2000 <aknowlti2000@gmail.com>
2024-10-29 01:15:59 -0400

    added testbench, still need to run and debug and figure out how to store .mem files

--- hdl_design/hdl_design.srcs/relu_tb/new/relu_tb.sv
+++ hdl_design/hdl_design.srcs/relu_tb/new/relu_tb.sv
@@ -1,14 +1,85 @@
 module relu_tb();
 
+    // N: Number of bits in a word
     parameter N=8;
-
+    
+    // L1: Number of words in input
+    parameter L1=1;
+    
+    // L2: Number of words in output
+    parameter L2 = 1;
+    
+    // initialize memories and counters
+    logic [$clog2(L1)-1:0] input_counter_n, input_counter_r;
+    logic [$clog2(L2)-1:0] output_counter_n, output_counter_r;
+    logic [N-1:0] input_test_vals [L1-1:0];
+    logic [N-1:0] output_test_vals [L2-1:0];
+    
+    initial begin
+        $readmemh("input.mem", input_test_vals);
+        $readmemh("expected.mem", output_test_vals);
+    end
+    
+    // declare variables for DUT
     logic valid_i, ready_o, yumi_i, valid_o;
-    logic signed [N-1:0] data_i, data_o;
+    logic signed [N-1:0] data_i, data_o, expected_data_o;
     logic rstb_i, clk_i;
     
-    // create send and receive modules with internal memory
+    // create send and receive modules locally
     // create DUT
-    // create clock module
+    relu_afb DUT(.*);
     
-
+    // create clock
+    parameter CLOCK_PERIOD = 10;
+    initial begin
+        clk_i = 1'b0;
+        forever #(CLOCK_PERIOD / 2) clk_i = ~clk_i;
+    end
+    
+    // handle stuff for address counters
+    assign input_counter_n = (valid_i && ready_o) && (input_counter_r != (L1)) ? 
+        input_counter_r + 1 : input_counter_r;
+    assign output_counter_n = valid_o && yumi_i && (output_counter_r != (L2)) ? 
+        output_counter_r + 1 : output_counter_r;
+    
+    // Create an LFSR to assign other two ready and valid signals
+    // this makes things more robust
+    logic [3:0] lfsr_n, lfsr_r;
+    assign lfsr_n = {lfsr_r[0]^lfsr_r[3], lfsr_r[3:1]};
+    assign yumi_i = lfsr_r[0] && (input_counter_r != L1);
+    assign valid_i = lfsr_r[1] && (output_counter_r != L2);
+    
+    assign data_i = input_test_vals[input_counter_r];
+    assign expected_data_o = output_test_vals[output_counter_r];
+    
+    always_ff @(posedge clk_i) begin
+        if (~rstb_i) begin
+            input_counter_r <= '0;
+            output_counter_r <= '0;
+            lfsr_r <= '0;
+        end else begin
+            input_counter_r <= input_counter_n;
+            output_counter_r <= output_counter_n;
+            lfsr_r <= lfsr_n;
+        end
+    end
+    
+    integer fd;
+    initial begin
+        fd = $fopen("output.csv", "w");
+        $fprintf(fd, "test_index,expected,received\n");
+        forever begin
+            @(negedge clk_i); // wait for the negative edge of the clock
+            
+            // reasoning: if the output counter has gone all the way up, we are done
+            // sending data and can do final checks
+            // should be able to check that valid_o is low
+            if (output_counter_r == L2) begin
+                $fclose(fd);
+                $stop;
+            end else if (valid_o && yumi_i) begin
+                $fprintf(fd,"%u,%d,%d\n", output_counter_r,expected_data_o,data_o);
+            end
+        end
+    end
 endmodule
