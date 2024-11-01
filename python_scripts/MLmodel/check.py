e10ccd03c6438f63821a278fae2d86742a3904aa
joshuaning <joshning@umich.edu>
2024-11-01 12:47:05 -0400

    done training

--- python_scripts/MLmodel/check.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-# path = "python_scripts\\MLmodel\\Dataset\\Split\\test_data\\df_test_data_1.txt"
-path = "python_scripts\\MLmodel\\Dataset\\df_test_data.txt"
-df = pd.read_csv(path, delim_whitespace=True, header=None)
-print(df.columns)
