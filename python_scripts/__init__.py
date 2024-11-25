4490a50dc46adb74d6f27cd7dc09e285eab083fa
Codax2000 <aknowlti2000@gmail.com>
2024-11-24 21:08:30 -0500

    still not able to import from other folders

--- python_scripts/__init__.py
+++
@@ -1,6 +0,0 @@
-# make utils available everywhere in the package
-from .utils import cordic_dnn_operations
-from .utils import CORDIC
-from .utils import fp_logic
-from .utils import write_mem_utils
-
---
+++ python_scripts/utils/__init__.py
@@ -0,0 +1,5 @@
+# make utils available everywhere in the package
+from .cordic_dnn_operations import *
+from .CORDIC import *
+from .fp_logic import *
+from .write_mem_utils import *
